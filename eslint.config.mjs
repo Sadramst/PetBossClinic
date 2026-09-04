9cd4201fb634fe36cdc7f59133d8a588ce9a21db
Sadra Tabarestani <m.sadra.mst@gmail.com>
2026-09-04 16:41:15 +0800

    Fix final ESLint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    ignores: ["*.js"],
+  },
+  {
     rules: {
       "no-console": ["warn", { allow: ["warn", "error"] }],
     },
